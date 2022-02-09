4d50da92f4be57fc7b5a6b732e7edbfbf4aa54d9
djalexkidd <alexpyr@outlook.fr>
2022-02-09 11:36:16 +0100

    Correction

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
+// On récupère les éléments du DOM
 const form = document.querySelector('.form');
-const taskList = document.querySelector('.list-items');
-const taskName = document.querySelector('input');
-const checked = "☑"
+const formInput = document.querySelector('.form input');
+const listItems = document.querySelector('.list-items');
+
+// On créé un objet qui va nous permettre d'exploiter des données
+let todoList = {
+    todo1: {
+        todo : 'Faire quelque chose'
+    },
+    todo2: {
+        todo : 'Manger'
+    },
+    todo3: {
+        todo : "S'inscrire sur Tinder"
+    }
+}
+
+// Boucler sur l'objet
+function loadHTML() {
+    if(!window.localStorage.getItem('data')) return
+    const data = JSON.parse(window.localStorage.getItem('data'))
+    todoList = data;
+    Object.keys(todoList).map(key => createHTML(todoList[key], key));
+}
+
+window.addEventListener('load', loadHTML);
 
 form.addEventListener('submit', createItem);
 
 function createItem(e) {
-    e.preventDefault(); // Ne recharge pas la page
+    e.preventDefault();
+    const timestamp = Date.now();
+    todoList[timestamp] = {
+        todo : formInput.value,
+        checked: false
+    }
+    createHTML(todoList[timestamp], timestamp);
+    saveObj();
+    this.reset();
+}
 
-    taskList.innerHTML += `
-    <li class="item">
-            <span>${taskName.value}</span>
+function createHTML(objet, key) {
+    if(!objet.todo) return;
+    const html = `
+            <span>${objet.todo}</span>
             <button name="trash" class="trash">🗑️</button>
-            <button class="check">☐</button>
-    </li>
-    `;
+            <button name="check" class="check">✔️</button>
+    `
 
-    updateCheck();
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.setAttribute('data-key', key);
+    li.innerHTML = html;
+    listItems.insertBefore(li, listItems.firstChild);
+
+    li.children.trash.onclick = toBin;
+    li.children.check.onclick = check;
 }
 
-function updateCheck() {
-    const checkBox = document.querySelectorAll('.check');
-    checkBox.forEach(element => {
-        element.addEventListener('click', () => {
-    
-            if (element.innerHTML === checked) {
-                element.innerHTML = "☐";
-            } else {
-                element.innerHTML = "☑";
-            }
-    
-        });
-    });
-
-    const trash = document.querySelectorAll('.trash');
-    trash.forEach(element => {
-        element.addEventListener('click', () => {
-    
-           deleteItem();
-    
-        });
-    });
+function toBin() {
+    this.parentNode.remove();
+    const key = this.parentNode.getAttribute('data-key');
+    delete todoList[key];
+    saveObj();
 }
 
-function deleteItem() {
-    console.log("Supprimé !");
+function check() {
+    this.innerHTML = this.innerHTML === '✔️' ? "🔄" : "✔️";
+    const key = this.parentNode.getAttribute('data-key');
+    todoList[key].checked = !todoList[key].checked;
+    saveObj();
+}
+
+function saveObj() {
+    window.localStorage.setItem('data', JSON.stringify(todoList));
 }
